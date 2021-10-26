d245953b8614ee457f67f4f017bd8a0e8a1eed3d
BellaHuiruTan <89065746+BellaHuiruTan@users.noreply.github.com>
2021-10-26 12:20:27 +0800

    amended nav bar

--- style.js
+++ style.js
@@ -59,26 +59,23 @@ app.component('navhead', {
         
     template: `
         <header id="header" class="d-flex align-items-center">
-          <div class="container d-flex align-items-center">
-        
-            <h1 class="logo me-auto"><a href="index.html">HealthYou</a></h1>
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-        
+            <div class="container d-flex align-items-center">
+
+            <h1 class="logo me-auto"><a href="index.html">HealthYou</a></h1>        
             <nav id="navbar" class="navbar">
-              <ul>
+                <ul>
                 <li><a class="nav-link scrollto active" href="index.html#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#index.htmlservices">Calories Tracker</a></li>
-                <li><a class="nav-link scrollto " href="index.html#portfolio">Portfolio</a></li>
+                <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
+                <li><a class="nav-link scrollto" href="index.html#services">Calories Tracker</a></li>
                 <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
-                <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
-                <li><a class="getstarted scrollto" href="index.html#about">Get Started</a></li>
-              </ul>
-              <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-          </div>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+
+            </div>
         </header>
             `
+           
 } )
 app.mount('#navHead')
 

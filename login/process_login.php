d734f5e567d5d2482906dcbdf14d2edb1d2659ea
Lim-Yi-Kiat <89069748+Lim-Yi-Kiat@users.noreply.github.com>
2021-11-07 19:19:02 +0800

    js session for some pages

--- login/process_login.php
+++ login/process_login.php
@@ -21,7 +21,7 @@
         if ($status)
         { 
             $_SESSION["username"] = $username;
-            header("Location: ../home.html");
+            header("Location: ../index.html");
             return;
         }
         else
@@ -47,3 +47,20 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <input type="hidden" id="username" value = "<?=$username?>">
+        <script>
+            var username = document.getElementById("username").value
+            localStorage.setItem("username", username);
+
+        </script>
+</body>
+</html>
